b269ad27d897939dd3cf46e2b4aac362e3bec476
blahxblah <id.blahxblah@gmail.com>
2023-09-05 09:06:37 +0900

    230905/blahxblah 정합성 체크, blob dag 작성

--- dags/blob.py
+++ dags/blob.py
@@ -3,15 +3,17 @@ from airflow.operators.empty import EmptyOperator
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import BranchPythonOperator
 from datetime import datetime, timedelta
-import pendulum
+from airflow.models.variable import Variable
+import pendulum, requests
 
 local_tz = pendulum.timezone('Asia/Seoul')
+SERVER_API = Variable.get("SERVER_API")
 
 # 프로젝트의 모든 DAG 공통 사항 기재
 default_args = {
     "owner" : "Hanul",
-    "depends_on_past" : True,
-    'start_date': datetime(2023, 8, 16, tzinfo=local_tz),
+    "depends_on_past" : False,
+    'start_date': datetime(2023, 9, 4, 15, 0, 0, tzinfo=local_tz),
     "retries" : 3,
     "retry_delay" : timedelta(seconds=15)
 }
@@ -23,14 +25,27 @@ dag = DAG(
     tags=['hanul', 'curl', 'gcs'],
     max_active_runs=1, # 동시에 실행되는 DAG의 수
     concurrency=1, # 동시에 실행되는 작업의 수
-    schedule_interval='*/30 * * * *',
-    user_defined_macros={},
+    schedule_interval='5 * * * *',
     default_args=default_args
 )
 
-start = EmptyOperator(
-    task_id="start",
-    dag=dag
-)
 
+def check_db():
+    api_url = f'http://{SERVER_API}/flags/404'
+    response = requests.get(api_url)
+    data = response.text
+    if data == '0':
+        return 'BLOB'
+    else:
+        return 'ERROR'
+    
+    
+start = EmptyOperator(task_id="start", dag=dag)
+check_flag = BranchPythonOperator(task_id='Check.Flag',python_callable=check_db, dag=dag)
+blob = BashOperator(task_id = "BLOB", bash_command= f'curl http://{SERVER_API}/blob/404', dag = dag)
+error = EmptyOperator(task_id = 'ERROR', dag = dag)
+end = EmptyOperator(task_id="end", dag=dag)
 
+start >> check_flag >> blob
+check_flag >> error
+blob >> end
